53d2d17620e180e31490e91fadae98f0dd23a0b8
Mike <mike@sachsmedia.com>
2014-06-25 15:50:11 -0400

    complete interior global style, multi-color page nav

--- wp-content/themes/fhca/page.php
+++ wp-content/themes/fhca/page.php
@@ -11,16 +11,17 @@ get_header(); ?>
 	<h1><?php echo get_the_title($post->post_parent);?></h1>
 </div>
 
-<div class="page-nav">
-	<?php 
-		if(wp_list_pages('child_of='.$post->ID.'&echo=0&exclude=2')) {
-			wp_list_pages('title_li=&child_of='.$post->ID.'&exclude=2');
-		} elseif(get_the_title($post->post_parent) != the_title(' ' , ' ',false)) {
-			wp_list_pages('child_of='.$post->post_parent.'&title_li=&exclude=2');
-		}
-	?>
-</div>
-	
+<div id="page-nav-background"></div>
+	<div class="page-nav">
+		<?php 
+			if(wp_list_pages('child_of='.$post->ID.'&echo=0&exclude=2')) {
+				wp_list_pages('title_li=&child_of='.$post->ID.'&exclude=2');
+			} elseif(get_the_title($post->post_parent) != the_title(' ' , ' ',false)) {
+				wp_list_pages('child_of='.$post->post_parent.'&title_li=&exclude=2');
+			}
+		?>
+	</div>
+
 </div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
