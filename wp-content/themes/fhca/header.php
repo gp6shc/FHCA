feea42a14a4ba7f9aeecb7b9d51bddf3b1b6d904
Mike <mike@sachsmedia.com>
2014-07-15 08:51:24 -0400

    initial home page design and structure

--- wp-content/themes/fhca/header.php
+++ wp-content/themes/fhca/header.php
@@ -12,7 +12,10 @@
 <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700|Crimson+Text:400,700' rel='stylesheet' type='text/css'>
 <title>FHCA <?php wp_title('|', TRUE, 'left' ); ?></title>
 
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<?php if( is_front_page() ) {?>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"> 
+<?}?>
+
 <?php wp_head(); ?>
 </head>
 
@@ -22,57 +25,63 @@
 	
 	<div id="header-wrapper">	
 		<header role="banner">
-			<div id="super-nav">
-			    <ul>
-			    	<!-- missing </li> tags to counter inline-block space -->
-			    	<li><a href="">Media Center</a>
-			    	<li><a href="">Contact</a>
-			    	<li class="search">
-			    	    <?php get_search_form(); ?>
-			    </ul>
+			<div class="header-super-contain">	
+				<div id="super-nav">
+				    <ul>
+				    	<!-- missing </li> tags to counter inline-block space -->
+				    	<li><a href="">Media Center</a>
+				    	<li><a href="">Contact</a>
+				    	<li class="search">
+				    	    <?php get_search_form(); ?>
+				    </ul>
+				</div>
 			</div>
-			
-			<div class="site-branding">
-			    <a href="<?php echo home_url(); ?>">
-			    <img src="<?php bloginfo('stylesheet_directory')?>/img/logo.svg" onerror="this.src="<?php bloginfo('stylesheet_directory')?>/img/logo.png"" alt="Florida Health Care Assocition">
-			    </a>
+
+			<div class="header-contain clearfix">
+				<div class="site-branding">
+				    <a href="<?php echo home_url(); ?>">
+				    <!-- <img src="<?php bloginfo('stylesheet_directory')?>/img/logo.svg" onerror="this.src="<?php bloginfo('stylesheet_directory')?>/img/logo.png"" alt="60 Years of Caring"> -->
+				    <div style="height: 74px; width: 74px; border-radius: 50%; background-color: lightblue; display: inline-block; "></div>
+				    <span style="color: white;">60 Years of Caring</span>
+				    </a>
+				</div>
+				
+				<nav role="navigation">
+				    <ul class="main-nav" id="elder-care">
+				    	<li><a href="<?php echo home_url(); ?>/florida-elder-care/infographic">Florida & Elder Care</a>
+				    		<ul class="sub-nav">
+				        		<?php wp_list_pages('title_li=&child_of=10'); ?>
+				    		</ul>
+				    	</li>
+				    </ul>
+				    
+				    <ul class="main-nav" id="aging">
+				        <li><a href="<?php echo home_url(); ?>/aging-issues/balance">Aging Issues</a>
+				        	<ul class="sub-nav">
+				    	    	<?php wp_list_pages('title_li=&child_of=12'); ?>
+				        	</ul>
+				        </li>
+				        	
+				    </ul>
+				    
+				    <ul class="main-nav" id="care-options">
+				        <li><a href="<?php echo home_url(); ?>/care-options/part-time-home-care">Care Options</a>
+				        	<ul class="sub-nav">
+				    	    	<?php wp_list_pages('title_li=&child_of=14'); ?>
+				        	</ul>
+				        </li>
+				    						    
+				    </ul>
+				    
+				    <ul class="main-nav" id="stories">
+				        <li><a href="<?php echo home_url(); ?>/stories/elder-care">Stories</a>
+				        	<ul class="sub-nav">
+				        		<?php wp_list_pages('title_li=&child_of=16'); ?>
+				    		</ul>
+				        </li>
+				    </ul>
+				</nav>
 			</div>
-			
-			<nav role="navigation">
-			    <ul class="main-nav" id="elder-care">
-			    	<li><a href="<?php echo home_url(); ?>/florida-elder-care/infographic">Florida & Elder Care</a>
-			    		<ul class="sub-nav">
-			        		<?php wp_list_pages('title_li=&child_of=10'); ?>
-			    		</ul>
-			    	</li>
-			    </ul>
-			    
-			    <ul class="main-nav" id="aging">
-			        <li><a href="<?php echo home_url(); ?>/aging-issues/balance">Aging Issues</a>
-			        	<ul class="sub-nav">
-			    	    	<?php wp_list_pages('title_li=&child_of=12'); ?>
-			        	</ul>
-			        </li>
-			        	
-			    </ul>
-			    
-			    <ul class="main-nav" id="care-options">
-			        <li><a href="<?php echo home_url(); ?>/care-options/part-time-home-care">Care Options</a>
-			        	<ul class="sub-nav">
-			    	    	<?php wp_list_pages('title_li=&child_of=14'); ?>
-			        	</ul>
-			        </li>
-			    						    
-			    </ul>
-			    
-			    <ul class="main-nav" id="stories">
-			        <li><a href="<?php echo home_url(); ?>/stories/elder-care">Stories</a>
-			        	<ul class="sub-nav">
-			        		<?php wp_list_pages('title_li=&child_of=16'); ?>
-			    		</ul>
-			        </li>
-			    </ul>
-			</nav>
 		</header>
 	</div>
 	
--- wp-content/themes/fhca/my-templates/home.php
+++ wp-content/themes/fhca/my-templates/home.php
@@ -1,56 +1,89 @@
 <?php
 /*
  * Template Name: Home
- * Description: Design of the home page
  */
 
 get_header(); ?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="<?php bloginfo('stylesheet_directory')?>/js/jquery.glide.min.js"></script>
-
-<div class="slider">
-	<ul class="slides">
-		<li class="slide">
-			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_1.jpg);">
-				
-			</div>
-		</li>
-		<li class="slide">
-			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_2.jpg);">
-				
-			</div>
-		</li>
-		<li class="slide">
-			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_3.jpg);">
-				
-			</div>
-		</li>
-	</ul>
-</div>		
-
-<script>
-    $(window).bind("load", function() {
-    	$('.slider').glide({
-    	    autoplay: 5000,
-    	    arrowLeftText: '<i class="fa fa-chevron-left fa-5x"></i>',
-    	    arrowRightText: '<i class="fa fa-chevron-right fa-5x"></i>'
-			});
-    	});
-</script>
 	
-	<div id="primary" class="content-area">
-		
-			<div style="height: 1200px; background-color: white;">
-				<p>Lorem ipsum dolor sit amet, sumo fugit assentior at mel. Eum ea feugiat perfecto, eum alterum euismod ei, usu an graeco consequat. Sit id quot mutat, vis ex iuvaret incorrupte. Cu usu summo tritani offendit.
-
-Accumsan aliquando id mea. Graece atomorum adipiscing ne qui, te eos choro voluptatum, in mei suas regione. Sit suas semper ut, zril homero dolorem ex pro. Modo tempor civibus at nec. Ad rebum possim pro, vel ullum iuvaret no, hinc quot discere nec ex.
-
-No qui pertinax oportere, usu adhuc fugit liberavisse in, sea no augue iudico detraxit. Zril decore cetero ad est, ius veri mnesarchum et. Nibh recteque in vis, mei dicat suavitate ea. Vel in adhuc veniam laudem, ne mea dicunt inimicus. Nulla labitur gloriatur eos no, vis ne facer lucilius contentiones.
-
-Autem laoreet imperdiet eu qui, quem nibh sanctus duo an. Menandri assentior has ut, usu malorum nusquam no, choro audiam nec in. Et alienum fastidii perpetua vix, ne pro omnis essent maiestatis. Qui ne zril percipit, putant alienum sententiae te vel. Qui amet assentior in. Altera voluptatum est at, his ad aeque senserit dignissim, ei prima periculis deseruisse mei.
-
-At has posse ullamcorper definitionem. Eu aliquip maiorum recusabo per. Vis quas falli alienum ex, in nec nonumes facilisi, eam ex lucilius probatus. Vocent timeam probatus ea duo. Sea sonet mollis efficiantur ad. Mei impetus dolores cu.</p>
+	<div id="home-contain" class="clearfix">
+		<div class="feature" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_1.jpg);">
+			<div>
+				<h2>Some News About Elders</h2>
+				<p>Xeriberibusae et as esequo blabore pudae. Ut laboreperum hit quunt dolumet, test, soloreribus, que pel ius voluptatur arum Xeriberibusae et as esequo blabore pudae.</p>
+				<button><a href="<?php echo home_url(); ?>/aging-issues/elder-stories">See Stories</a></button>
 			</div>
+		</div>
+		
+		<aside>
+			<h2>Aging Issues</h2>
+			<ul>
+				<li>
+					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/balance">
+					<div class="fa-stack fa-2x">	
+						<i class="fa fa-child fa-stack-1x"></i>
+						<i class="fa fa-circle-thin fa-stack-2x"></i>
+					</div>
+					<h6>Balance</h6>
+					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco </p>
+					</a>
+				</li>
+				<li>
+					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/depression">
+					<div class="fa-stack fa-2x">	
+						<i class="fa fa-arrow-circle-down fa-stack-1x"></i>
+						<i class="fa fa-circle-thin fa-stack-2x"></i>
+					</div>
+					<h6>Depression</h6>
+					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					</a>
+				</li>
+				<li>
+					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/driving">
+					<div class="fa-stack fa-2x">	
+						<i class="fa fa-car fa-stack-1x"></i>
+						<i class="fa fa-circle-thin fa-stack-2x"></i>
+					</div>
+					<h6>Driving</h6>
+					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					</a>
+				</li>
+				<li>
+					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/hearing">
+					<div class="fa-stack fa-2x">	
+						<i class="fa fa-volume-up fa-stack-1x"></i>
+						<i class="fa fa-circle-thin fa-stack-2x"></i>
+					</div>
+					<h6>Hearing</h6>
+					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					</a>
+				</li>
+				<li>
+					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/independence">
+					<div class="fa-stack fa-2x">	
+						<i class="fa fa-road fa-stack-1x"></i>
+						<i class="fa fa-circle-thin fa-stack-2x"></i>
+					</div>
+					<h6>Independence</h6>
+					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					</a>
+				</li>
+			</ul>
+				<button><a href="<?php echo home_url(); ?>/aging-issues/medication">See More</a></button>
+		</aside>
+		
+		<div class="boxes">
+			<div class="box">
+			    <div><span>60 Years of Caring Report</span></div>
+			</div>
+			<div class="box">
+			    <div><span>60 Years of Caring Report</span></div>
+			</div>
+			<div class="box">
+			    <div><span>60 Years of Caring Report</span></div>
+			</div>
+		</div>
+	</div>	
+				
 
 
 <?php get_footer(); ?>
