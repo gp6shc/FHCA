7458558dfaecaa928cc6d5c7be40fd735e01e97d
Mike <mike@sachsmedia.com>
2014-07-15 14:37:08 -0400

    reduced comment bloat

--- wp-content/themes/fhca/search.php
+++ wp-content/themes/fhca/search.php
@@ -20,11 +20,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
+				
 				get_template_part( 'content', 'search' );
 				?>
 			<?php endwhile;  ?>
