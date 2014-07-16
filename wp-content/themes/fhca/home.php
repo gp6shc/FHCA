5576c6fd9e9292d7e2e9a7e3510d1fcfdc7c7f51
Mike <mike@sachsmedia.com>
2014-07-16 17:33:17 -0400

    home page responsive design

--- wp-content/themes/fhca/home.php
+++ wp-content/themes/fhca/home.php
@@ -8,78 +8,77 @@ get_header(); ?>
 	<div id="home-contain" class="clearfix">
 		<div class="feature" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_1.jpg);">
 			<div>
-				<h2>Some News About Elders</h2>
-				<p>Xeriberibusae et as esequo blabore pudae. Ut laboreperum hit quunt dolumet, test, soloreribus, que pel ius voluptatur arum Xeriberibusae et as esequo blabore pudae.</p>
-				<button><a href="<?php echo home_url(); ?>/aging-issues/elder-stories">See Stories</a></button>
+				<?php $id = 6; $post_object = get_post( $id ); echo $post_object->post_content; ?>
+				<button><a href="<?php echo get_permalink(30); ?>">See Stories</a></button>
 			</div>
 		</div>
 		
 		<aside>
-			<h2>Aging Issues</h2>
+			<h2><?php echo get_the_title(12); ?></h2>
 			<ul>
 				<li>
-					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/balance">
+					<a class="clearfix" href="<?php echo get_permalink(20); ?>">
 					<div class="fa-stack fa-2x">	
 						<i class="fa fa-child fa-stack-1x"></i>
 						<i class="fa fa-circle-thin fa-stack-2x"></i>
 					</div>
-					<h6>Balance</h6>
-					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco </p>
+					<h6><?php echo get_the_title(20); ?></h6>
+					<p><?php echo the_post_excerpt_by_id(20,20);?></p>
 					</a>
 				</li>
 				<li>
-					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/depression">
+					<a class="clearfix" href="<?php echo get_permalink(42); ?>">
 					<div class="fa-stack fa-2x">	
 						<i class="fa fa-arrow-circle-down fa-stack-1x"></i>
 						<i class="fa fa-circle-thin fa-stack-2x"></i>
 					</div>
-					<h6>Depression</h6>
-					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					<h6><?php echo get_the_title(42); ?></h6>
+					<p><?php echo the_post_excerpt_by_id(42,20);?></p>
 					</a>
 				</li>
 				<li>
-					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/driving">
+					<a class="clearfix" href="<?php echo get_permalink(45); ?>">
 					<div class="fa-stack fa-2x">	
 						<i class="fa fa-car fa-stack-1x"></i>
 						<i class="fa fa-circle-thin fa-stack-2x"></i>
 					</div>
-					<h6>Driving</h6>
-					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					<h6><?php echo get_the_title(45); ?></h6>
+					<p><?php echo the_post_excerpt_by_id(45,20);?></p>
 					</a>
 				</li>
 				<li>
-					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/hearing">
+					<a class="clearfix" href="<?php echo get_permalink(47); ?>">
 					<div class="fa-stack fa-2x">	
 						<i class="fa fa-volume-up fa-stack-1x"></i>
 						<i class="fa fa-circle-thin fa-stack-2x"></i>
 					</div>
-					<h6>Hearing</h6>
-					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					<h6><?php echo get_the_title(47); ?></h6>
+					<p><?php echo the_post_excerpt_by_id(47,20);?></p>
 					</a>
 				</li>
 				<li>
-					<a class="clearfix" href="<?php echo home_url(); ?>/aging-issues/independence">
+					<a class="clearfix" href="<?php echo get_permalink(49); ?>">
 					<div class="fa-stack fa-2x">	
 						<i class="fa fa-road fa-stack-1x"></i>
 						<i class="fa fa-circle-thin fa-stack-2x"></i>
 					</div>
-					<h6>Independence</h6>
-					<p>Que vellendi consed ma gnamet, ipsant ad maxim quam que odigenempeco</p>
+					<h6><?php echo get_the_title(49); ?></h6>
+					<p><?php echo the_post_excerpt_by_id(49,20);?></p>
 					</a>
 				</li>
 			</ul>
-				<button><a href="<?php echo home_url(); ?>/aging-issues/medication">See More</a></button>
+				<button><a href="<?php echo get_permalink(51); ?>">See More</a></button>
 		</aside>
 		
 		<div class="boxes">
 			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_1.jpg);" >
-			    <a href="<?php echo home_url(); ?>/florida-elder-care/60-years-of-caring-report/"><span>60 Years of Caring Report</span></a>
+			    <a href="<?php echo get_permalink(33); ?>"><span><?php echo get_the_title(33); ?></span></a>
 			</div>
 			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_2.jpg);">
-			    <a href="<?php echo home_url(); ?>/florida-elder-care/60-years-of-caring-report/"><span>60 Years of Caring Report</span></a>
+			    <a href="<?php echo get_permalink(33); ?>"><span><?php echo get_the_title(33); ?></span></a>
 			</div>
 			<div class="box" style="background-image: url(<?php bloginfo('stylesheet_directory')?>/img/fhca_elderly_3.jpg);">
-			    <a href="<?php echo home_url(); ?>/florida-elder-care/60-years-of-caring-report/"><span>60 Years of Caring Report</span></a>
+			    <a href="<?php echo get_permalink(33); ?>"><span><?php echo get_the_title(33); ?></span></a>
 			</div>
 		</div>
 	</div>	
