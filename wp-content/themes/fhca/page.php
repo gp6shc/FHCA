c6c606a816bb78f427e358826412d4add3fd3c3a
Mike <mike@sachsmedia.com>
2014-07-17 17:47:31 -0400

    began new interior layout

--- wp-content/themes/fhca/page.php
+++ wp-content/themes/fhca/page.php
@@ -7,40 +7,33 @@
 
 get_header(); ?>
 
-<div class="title">
-	<h1><?php echo get_the_title($post->post_parent);?></h1>
-</div>
-
-<!--
-<div id="page-nav-background"></div>
-	<div class="page-nav">
-		<?php 
-			if(wp_list_pages('child_of='.$post->ID.'&echo=0&exclude=2')) {
-				wp_list_pages('title_li=&child_of='.$post->ID.'&exclude=2');
-			} elseif(get_the_title($post->post_parent) != the_title(' ' , ' ',false)) {
-				wp_list_pages('child_of='.$post->post_parent.'&title_li=&exclude=2');
-			}
-		?>
-	</div>
-</div>
--->
-<div class="side-nav">
-	<?php 
-		if(wp_list_pages('child_of='.$post->ID.'&echo=0&exclude=2')) {
-			wp_list_pages('title_li=&child_of='.$post->ID.'&exclude=2');
-		} elseif(get_the_title($post->post_parent) != the_title(' ' , ' ',false)) {
-			wp_list_pages('child_of='.$post->post_parent.'&title_li=&exclude=2');
-		}
-	?>
-</div>
-
 
 	<div id="primary" class="content-area">
+		
 		<main id="main" class="site-main" role="main">
+			
+			<div class="side-nav">
+				<?php 
+					if(wp_list_pages('child_of='.$post->ID.'&echo=0&exclude=2')) {
+						wp_list_pages('title_li=&child_of='.$post->ID.'&exclude=2');
+					} elseif(get_the_title($post->post_parent) != the_title(' ' , ' ',false)) {
+						wp_list_pages('child_of='.$post->post_parent.'&title_li=&exclude=2');
+					}
+				?>
+			</div>
+	
 			<?php while ( have_posts() ) : the_post(); ?>
- 
-				<?php get_template_part( 'content', 'page' ); ?>
- 
+				
+				<?php get_template_part( 'content', 'page' );
+				
+				$thumb_id = get_post_thumbnail_id();
+				$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+				$thumb_url = $thumb_url_array[0]; ?>
+				
+			<div class="side-hero" style="background-image: url(<?php $thumb_url?>)">
+				
+			</div>
+				
 			<?php endwhile; // end of the loop. ?>
 			
 
