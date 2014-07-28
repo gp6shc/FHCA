f33d613a0739dee9a865697c2b9cb3a29f4320da
Mike <mike@sachsmedia.com>
2014-07-28 19:01:05 -0400

    redirect infographics page to its first child

--- wp-content/themes/fhca/redirect-to-first-child.php
+++ wp-content/themes/fhca/redirect-to-first-child.php
@@ -2,11 +2,5 @@
 /*
 Template Name: Redirect to first child
 */
-	$pagekids = get_pages("child_of=".$post->ID."&sort_column=menu_order");
-	if ($pagekids) {
-		$firstchild = $pagekids[0];
-		wp_redirect(get_permalink($firstchild->ID));
-	} else {
-		wp_redirect(get_permalink(0);
-	}
+wp_redirect(get_permalink(444));
 ?>
