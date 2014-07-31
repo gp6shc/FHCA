6fd8e1924483962377bfdf6e0da128e6dd8fce1e
Mike <mike@sachsmedia.com>
2014-07-31 14:28:56 -0400

    commenting js and added GA

--- wp-content/themes/fhca/footer.php
+++ wp-content/themes/fhca/footer.php
@@ -43,14 +43,18 @@
 
 <?php wp_footer(); ?>
 <script type="text/javascript">
+	
+	// initiate FastClick.js whenpage loads
 	window.addEventListener('load', function() {
 		FastClick.attach(document.body);
 	}, false);
 	
+	// Get the mobile menu icons and nav element 
 	var button = document.getElementById('menu-button');
 	var navBar = document.getElementById('nav-bar');
 	var	faNavIcon = button.children;
 	
+	// Listen for touch events on the mobile menu; toggle the nav height and icons
 	button.addEventListener( 'touchstart', function() {
 		navBar.classList.toggle('height');
 		faNavIcon[0].classList.toggle('hidden');
@@ -58,11 +62,22 @@
 		
 	}, false);
 	
+	// Listen for mouse clicks on thh mobile menu
 	button.addEventListener( 'mousedown', function() {
 		navBar.classList.toggle('height');
 		faNavIcon[0].classList.toggle('hidden');
 		faNavIcon[1].classList.toggle('hidden');
 	}, false);
+	
+	//Google Analytics
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	ga('create', 'UA-53377212-1', 'auto');
+	ga('send', 'pageview');
 </script>
+
 </body>
 </html>
